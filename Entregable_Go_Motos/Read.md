d4145c4b235b5dbda111e15c1208e13409db37c8
matiasgandara <gandaramatias@gmail.com>
2020-12-02 20:51:20 -0300

    Se cargo correctamente la tabla y unica carga de moto

---
+++ Entregable_Go_Motos/Read.md
@@ -0,0 +1,4 @@
+# Crud completo, 
+
+
+# El error estaba en la carga de la base de datos.... no estaba siendo llamada
--- Entregable_Go_Motos/cmd/motos/motossrv.go
+++ Entregable_Go_Motos/cmd/motos/motossrv.go
@@ -6,7 +6,6 @@ import (
 	"os"
 
 	"github.com/gin-gonic/gin"
-	"github.com/jmoiron/sqlx"
 	"github.com/matiasgandara/GoLang/tree/main/Entregable_Go_Motos/internal/config"
 	"github.com/matiasgandara/GoLang/tree/main/Entregable_Go_Motos/internal/database"
 	"github.com/matiasgandara/GoLang/tree/main/Entregable_Go_Motos/internal/service/motos"
@@ -17,8 +16,8 @@ func main() {
 
 	// Instanciacion de db
 	db, err := database.NewDatabase(cfg)
-	defer db.Close()
-
+	/* 	defer db.Close()
+	 */
 	if err != nil {
 		fmt.Println(err.Error())
 		os.Exit(1)
@@ -44,26 +43,4 @@ func readConfig() *config.Config {
 	}
 
 	return cfg
-}
-
-func createSchema(db *sqlx.DB) error {
-	schema := `CREATE TABLE IF NOT EXISTS  motos (
-    id integer NOT NULL CONSTRAINT motos_pk PRIMARY KEY AUTOINCREMENT,
-	patente varchar(6) NOT NULL,
-    modelo integer NOT NULL,
-	nombre varchar(30) NOT NULL,
-    cilindrada integer NOT NULL,
-	color varchar(30) NOT NULL,
-    );`
-	// execute query on server
-	_, err := db.Exec(schema)
-	if err != nil {
-		return err
-	}
-
-	//or, you can use MustExec, which panics on error
-	insertMoto := `INSERT INTO motos (patente, modelo, nombre, cilindrada, color) VALUES(?,?,?,?,?)`
-	s := fmt.Sprintf("ABC123", 2015, "Tornado", 250, "azul")
-	db.MustExec(insertMoto, s)
-	return nil
 }
